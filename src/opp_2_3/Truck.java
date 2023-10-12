2470b77ca3cc32ba9d7a61fb0050bf96d3131215
Valerii <lord_liret@mail.ru>
2023-10-12 19:27:30 +0500

    Внесены запрошенные изменения.

--- src/opp_2_3/Truck.java
+++ src/opp_2_3/Truck.java
@@ -8,10 +8,12 @@ public class Truck extends Car {
     public void checkTrailer() {
         System.out.println("Проверяем прицеп");
     }
+
     @Override
     public void checkEngine() {
         System.out.println("Проверяем двигатель");
     }
+
     @Override
     public void service() {
         super.service();
